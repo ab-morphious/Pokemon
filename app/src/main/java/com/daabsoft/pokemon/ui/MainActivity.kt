d4bfa82db9ce06d52e7097fd5a99a709ef61a74c
abel <abel@kemecode.com>
2023-05-30 20:11:09 +0300

    setup: add project structure skeleton

--- app/src/main/java/com/daabsoft/pokemon/ui/MainActivity.kt
+++ app/src/main/java/com/daabsoft/pokemon/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.daabsoft.pokemon
+package com.daabsoft.pokemon.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.daabsoft.pokemon.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
